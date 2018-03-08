b71c62bc74db37618fe0e1ea5ac2f51a377dfba4
Lazar Stijakovic <zomlaaa95@gmail.com>
2018-03-08 14:52:46 +0100

    ui structure change

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -6,13 +6,19 @@ import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- public/src/app/app.routing.ts
+++ public/src/app/app.routing.ts
@@ -4,9 +4,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const appRoutes: Routes = [
     // { path: '', component: HomeComponent }
+    { path: 'client/login', component: LoginComponent },
+    { path: 'manager/login', component: LoginComponent }
 ];
 
 export const routing = RouterModule.forRoot(appRoutes, { useHash: true });
