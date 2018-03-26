daf2ae68bc36ba159ae97d242f2dd63dfb3d7935
Lazar Stijakovic <zomlaaa95@gmail.com>
2018-03-26 12:25:32 +0200

    some work on destination page

--- public/src/app/app.routing.ts
+++ public/src/app/app.routing.ts
@@ -14,6 +14,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UpdateCmInfoComponent } from './update-cm-info/update-cm-info.component';
 import { UsersComponent } from './users/users.component';
 import { DestinationsComponent } from './destinations/destinations.component';
+import { DestinationComponent } from './destination/destination.component';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
@@ -76,7 +77,7 @@ const appRoutes: Routes = [
                     },
                     {
                         path: 'destinations/:id',
-                        component: LoginComponent
+                        component: DestinationComponent
                     },
                     {
                         path: 'destinationRequests',
