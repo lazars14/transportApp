b97d61620dd226cc8c118bffff506268a8301910
Lazar Stijakovic <zomlaaa95@gmail.com>
2018-05-31 10:01:26 +0200

    added config for production

---
+++ config/config.prod.js
@@ -0,0 +1,33 @@
+var config = {
+    env: 'prod',
+    server: 'http://www.diplomskilazarstijakovic.in.rs/',
+    token: {
+      secret: '123&$&*!010293123mxan!#'
+    },
+    firebaseServerKey: 'AIzaSyDiiYDU2ySFY7rpXMmd3rSIqYgHDGI7kVI',
+    logger: {
+      path: './log/server.log'
+    },
+    log: {
+      "level" : "info",
+      "json" : false,
+      "logsToFile" : true,
+      "fileLogParams" : {
+        "folder" : "log",
+        "prefix" : "transportApp_test",
+        "datePattern" : "yyyy_MM_dd",
+        "extension" : "log",
+        "keepLogsDays" : 3
+      },
+      "logsToDatabase": false
+    },
+    mongodb: {
+      host: "mongodb://localhost:27017/",
+      db: "transportApp_test",
+      debug: false
+    },
+    // http://localhost:9000
+    "serverURL": "http://www.diplomskilazarstijakovic.in.rs/"
+  };
+  
+  module.exports = config;
